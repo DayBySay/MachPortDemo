56e16215a380869efba76995d6dd3f83b4b43891
Takayuki Sei <takayuki.sei@gree.net>
2021-12-21 00:31:08 +0900

    initial commit

---
+++ MachPortDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MachPortDemo
+//
+//  Created by Takayuki Sei on 2021/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
